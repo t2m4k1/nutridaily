3a000fe3a026d9fdb6752f1ce4b154273b7575a1
Thomas H <thomash241@gmail.com>
2018-04-16 11:55:10 +0200

    Added Profile Highlighting In NavBar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,12 @@
 module ApplicationHelper
     def current_class?(test_path)
-        request.path == test_path ? 'active' : ''
+        unless test_path.instance_of? String
+            test_path = url_for(test_path);
+        end
+        if request.path == test_path
+            'active'
+        else
+            ''
+        end
     end
 end
