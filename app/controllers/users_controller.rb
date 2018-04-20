81fb5b9fbcd7c34f093e6dc4d8db8e191f84ee1d
Thomas H <thomash241@gmail.com>
2018-04-20 10:29:36 +0200

    Implemented Friendly Forwarding

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:edit, :update]
+  before_action :logged_in_user, only: [:show, :edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
 
   def show
     @user = User.find(params[:id])
@@ -15,23 +16,23 @@ class UsersController < ApplicationController
   end
 
   def update
-    @user = User.find(params[:id])
-    if @user.update_attributes(user_params)
-        @user.forget_all_tokens
+    user = User.find(params[:id])
+    if user.update_attributes(user_params)
+        user.forget_all_tokens
         flash[:success] = "Profile updated"
-        redirect_to @user
+        redirect_to user
     else
       render 'edit'
     end
   end
 
   def create
-    @user = User.new(user_params)
-    if @user.save
-        log_in @user
-        redirect_to @user
+    user = User.new(user_params)
+    if user.save
+        log_in user
+        redirect_back user
     else
-        flash.now[:danger] = 'User can\'t be registered!'
+        flash.now[:danger] = 'User can\'t log in'
         render 'new'
     end
   end
@@ -44,8 +45,14 @@ class UsersController < ApplicationController
 
   def logged_in_user
     unless logged_in?
+      store_location
       flash[:danger] = "Please log in."
       redirect_to login_url
     end
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless current_user?(@user)
   end
 end
