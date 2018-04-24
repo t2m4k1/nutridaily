ffd9e2576ca69592b2a0c169364ecf9b652d4161
Thomas H <thomash241@gmail.com>
2018-04-24 20:14:15 +0200

    Changed Ingredient Show.html

--- app/uploaders/image_file_uploader.rb
+++ app/uploaders/image_file_uploader.rb
@@ -23,6 +23,7 @@ class ImageFileUploader < CarrierWave::Uploader::Base
 
   # Process files as they are uploaded:
   # process scale: [200, 300]
+    process resize_to_fill: [600,600]
   #
   # def scale(width, height)
   #   # do something
